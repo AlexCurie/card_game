4184c3fa79b303ef97bf3863c4dc53bc85d73d8d
nahorc <gusgravpop@gmail.com>
2023-05-16 13:35:36 +0300

    test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import javax.swing.*;
+public class Main{
+    public static void main(String args[]) {
+        JFrame a = new JFrame("example");
+        JButton b = new JButton("click me");
+        b.setBounds(40,90,85,20);
+        a.add(b);
+        a.setSize(300,300);
+        a.setLayout(null);
+        a.setVisible(true);
     }
 }
